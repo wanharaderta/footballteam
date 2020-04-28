529f8141520ac786a7a8ca420a153366b976723e
wanhar <wanhar@sehatq.com>
2020-04-28 11:15:45 +0700

    Initial Commit

---
+++ football/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  football
+//
+//  Created by Wanhar on 28/04/20.
+//  Copyright © 2020 Wanhar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
